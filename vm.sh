1b45bac3b0ec658edc2b42c1c205a466a03890d1
Jon Warren <jon@jonwarren.info>
2016-06-11 16:11:36 -0500

    Updated removal script to be a bit more robust with argument validation

--- vm.sh
+++ vm.sh
@@ -4,38 +4,62 @@ vimbaks=*.vim~
 dotfiles=.${vimbaks}
 args=""
 
+# default to interactive removal current-only
 if [ -z "$1" ]; then
   args="-i"
-elif [ $(expr index "$1" "-") -eq 1 ]; then
-  args=$1
 else
-  echo "Error: Unknown option: $1"
-  exit
+  args="$1"
 fi
 
+# forced removal recursive 
 if [ "$args" == "-rf" ] || [ "$args" == "-fr" ]; then
   find . -type f -name \*.vim~ -delete
   find . -type f -name .\*.vim~ -delete
-elif [ "$args" == "-r" ]; then
+
+# interactive removal recursive 
+elif [ "$args" == "-r" ] || [ "$args" == "-ri" ] || [ "$args" == "-ir" ]; then
   find . -type f -name \*.vim~ -exec rm -i {} \;
   find . -type f -name .\*.vim~ -exec rm -i {} \;
-else
+
+# listing recursive 
+elif [ "$args" == "-rl" ] || [ "$args" == "-lr" ]; then
+  find . -type f -name \*.vim~ -print
+  find . -type f -name .\*.vim~ -print
+
+# listing current-only 
+elif [ "$args" == "-l" ]; then
   for f in ${dotfiles}; do
-    if [ "${args}" == "-l" ]; then
-      [ -e "$f" ] && /usr/bin/ls ${dotfiles}
-    else
-      [ -e "$f" ] && /usr/bin/rm ${args} ${dotfiles}
-    fi
+    [ -e "$f" ] && /usr/bin/ls ${dotfiles}
     break
   done
   for f in ${vimbaks}; do
-    if [ "${args}" == "-l" ]; then
-      [ -e "$f" ] && /usr/bin/ls ${vimbaks}
-    else
-      [ -e "$f" ] && /usr/bin/rm ${args} ${vimbaks}
-    fi
+    [ -e "$f" ] && /usr/bin/ls ${vimbaks}
     break
   done
+
+# interactive or forced removal current-only 
+elif [ "$args" == "-i" ] || [ "$args" == "-f" ]; then
+  for f in ${dotfiles}; do
+    [ -e "$f" ] && /usr/bin/rm ${args} ${dotfiles}
+    break
+  done
+  for f in ${vimbaks}; do
+    [ -e "$f" ] && /usr/bin/rm ${args} ${vimbaks}
+    break
+  done
+
+# help info
+else
+  echo "Error: Unknown option: $args"
+  echo "Usage: $0 [-i]        # interactive removal in current directory"
+  echo "       $0 -f          # forced removal in current directory"
+
+  echo "       $0 -l          # list files in current directory"
+  echo "       $0 -lf|-fl     # list files in current tree"
+
+  echo "       $0 -r|-ri|-ir  # recursive interactive removal in current tree"
+  echo "       $0 -rf|-fr     # forced removal in current tree"
+  exit
 fi
 
-# vim: filetype=sh
+# vim: filetype=sh ts=2 sw=2
